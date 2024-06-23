162099aa30e10234d45bc47404181dcf85ba6745
eunbin222 <106373940+eunbin222@users.noreply.github.com>
2024-06-23 23:47:24 +0900

    Update portfolio.js

--- js/portfolio.js
+++ js/portfolio.js
@@ -40,7 +40,7 @@
 	});
 	
 	//상단 한글자씩 나오는 기능 (타자치는 효과)
-	const typing = "Hi ~ \n My name is EunBin.\n I'm a Web designer !"
+	const typing = "Hi ~ \n My name is EunBin."
 	console.log(typing);
 	//$("h1").html( typing[4]  );  "요"
 	let i = 0; 
